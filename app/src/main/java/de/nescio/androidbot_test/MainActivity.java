bc39917118992f0e1c7c20d3d3f9ece642e8d2da
Gökhan Kalender <gkalender@cellular.de>
2014-11-14 21:25:24 +0100

    update

--- app/src/main/java/de/nescio/androidbot_test/MainActivity.java
+++ app/src/main/java/de/nescio/androidbot_test/MainActivity.java
@@ -2,38 +2,43 @@ package de.nescio.androidbot_test;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.nescio.androidbottest.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button mInjectButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mInjectButton = (Button) findViewById(R.id.inject_btn);
+        mInjectButton.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void startEventInject() {
+        mTextView.requestFocus();
+        final Thread t = new Thread() {
+            public void run() {
+            }
+        };
+        t.start();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        if (view == mInjectButton) {
+            startEventInject();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/de/nescio/androidbot_test/ServiceManager.java
@@ -0,0 +1,9 @@
+package de.nescio.androidbot_test;
+
+import android.os.IBinder;
+
+public class ServiceManager {
+    public static IBinder getService( String serviceName ) {
+        return null;
+    }
+}
